797aaa454e60c5700a9d034da596ab9b23ad0704
Tom lima <dev@tomlima.me>
2023-11-09 09:13:51 -0300

    Add web development support

--- init.el
+++ init.el
@@ -28,3 +28,51 @@
 
 ;; Display line numbers
 (global-display-line-numbers-mode)
+
+
+;; Inihibit cl warning
+(setq byte-compile-warnings '(cl-functions))
+
+;; Packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (prettier web-mode markdown-mode emmet-mode js2-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Allow use of emmet mode in an html or css buffer
+(require 'emmet-mode)
+
+;; web mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+
+(defun my-web-mode-hook()
+  "Hooks for web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  )
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+;; Change spacing at top of buffer
+(add-to-list 'default-frame-alist '(internal-border-width . 7))
