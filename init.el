4c85ccd7d122435eca7f42f7589611eb74a4d51e
Tom Lima <tom@tomlima.me>
2024-05-29 18:57:37 -0300

    Add line highlight

--- init.el
+++ init.el
@@ -1,8 +1,24 @@
 ;; Stops startup message
 (setq inhibit-splash-screen t)
 
+;; Tree view
+(global-set-key (kbd "<f9>") 'dir-treeview)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Font size
+(set-face-attribute 'default nil :height 115)
+
+
 ;; Maximize screen on startup
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Set default folder
+(cd "C:\\Users\\TomLima\\repos" )
+
+;; Set Margins
+(set-window-margins (selected-window) 8 8)
 
 ;; Disable menu on startup
 (menu-bar-mode -1)
@@ -24,7 +40,7 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 ;; Load custom theme
-(load-theme 'dracula t)
+(load-theme 'granger t)
 
 ;; Display line numbers
 (global-display-line-numbers-mode)
@@ -44,15 +60,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dir-treeview-default-root "C:\\Users\\TomLima\\repos")
  '(package-selected-packages
-   (quote
-    (prettier web-mode markdown-mode emmet-mode js2-mode))))
+   '(sublime-themes dir-treeview-themes forest-blue-theme dir-treeview boron-theme prettier web-mode markdown-mode emmet-mode js2-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:background nil))))
+ '(dir-treeview-archive-face ((t (:inherit dir-treeview-default-filename-face :foreground "dark slate gray"))))
+ '(dir-treeview-directory-face ((t (:inherit dir-treeview-default-filename-face :foreground "green"))))
+ '(hl-line ((t (:extend t :background "dark slate gray" :distant-foreground "snow" :foreground "snow"))))
+ '(isearch ((t (:background "blue" :foreground "green" :weight bold)))))
 
 ;; Allow use of emmet mode in an html or css buffer
 (require 'emmet-mode)
@@ -64,6 +84,9 @@
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
 
 (defun my-web-mode-hook()
   "Hooks for web mode."
